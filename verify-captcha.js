5915c76e7b81889728cbfc895ea3f1b49ddaffe2
sigmasonic3012-del <sigmasonic3012@gmail.com>
2025-11-22 23:52:04 -0800

    Add files via upload

--- verify-captcha.js
+++ verify-captcha.js
@@ -5,7 +5,7 @@ exports.handler = async function(event, context) {
   const captcha = body["g-recaptcha-response"];
 
   // Напишите код здесь: вставьте ваш Secret Key
-  const secretKey = "6Le3lRUsAAAAADaelfMhsIlJE_Gw-RigmKNs0TLG";
+  const secretKey = "6LcXtBUsAAAAAPI3wRVkhqugk7K89pCFoidA0OvT";
 
   const verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}`;
   const response = await fetch(verificationUrl, { method: "POST" });
